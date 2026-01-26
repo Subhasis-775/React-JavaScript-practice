9c9660dfc50780819c8af0c0dea4c664f2c89de7
Subhasis <subhasisrout00@gmail.com>
2026-01-26 09:04:49 +0530

    Added Expense Tracker

---
+++ Expense-Tracker/src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ExpenseTracker from "./ExpenseTracker";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ExpenseTracker />
+    </div>
+  );
+}
